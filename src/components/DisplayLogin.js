25e12bfe064d53be224a407309718686261f6873
CarlosEscat <carlosescat@gmail.com>
2019-08-13 15:02:33 +0200

    Router and components added

---
+++ src/components/DisplayLogin.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class DisplayLogin extends React.Component {
+  onSubmit() {}
+
+  onChange() {}
+
+  render() {
+    return (
+      <div>
+        <h2>Log In with name and password</h2>
+
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <p>Name:</p>
+            <input type="text" onChange={this.onChange} />
+            <p>Password:</p>
+            <input type="text" onChange={this.onChange} />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
