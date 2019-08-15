0abccbfd6e0749667a2d487300bb0d6377ee50bf
MerylGeugies <mgeugies@gmail.com>
2019-08-15 13:42:30 +0200

    Implemented the list of rooms and the stream

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -2,27 +2,31 @@ import React from "react";
 import * as request from "superagent";
 import { url } from "../constants";
 import { allRooms } from "../actions";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 
-export default class CreateRoom extends React.Component {
+class CreateRoom extends React.Component {
   state = { room_name: '' }
 
   onSubmit = async (event) => {
     event.preventDefault()
 
-    await request 
+    const { room_name } = this.state;
+
+    const response = await request 
       .post(`${url}/room`)
-      .send( { room_name: this.state.room_name })
-      .then(response => {
-        this.setState({ room_name: '' });
-        console.log('this.state', this.state)
-      });
+      .send({ room_name })
+
+    console.log('response test:', response)
+
+    this.setState({ room_name: '' });
+    console.log('this.state', this.state)
   }
 
   onChange = (event) => {
     const { value } = event.target
 
     this.setState({ room_name: value })
+    console.log('this.setState', this.setState)
   }
 
   render() {
@@ -40,14 +44,16 @@ export default class CreateRoom extends React.Component {
               onChange={this.onChange} />
             <button type="submit"> Add Room </button>
           </form>
-
         </div>
-
-        <div>
-          {allRooms}
-        </div>
-      
       </div>
     );
   }
 }
+const mapDispatchToProps = {
+  allRooms
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateRoom)
---
+++ src/components/DisplayRooms.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { connect } from 'react-redux'
+import CreateRoom from './CreateRoom'
+
+class DisplayRooms extends React.Component {
+  render () {
+    console.log('this.props test:', this.props)
+    const rooms = this
+      .props
+      .rooms
+      .map(room => 
+      <div
+        key={room.id}
+        to={'/room'}
+      >
+        <div>{room.room_name}</div>
+      </div>)
+
+    console.log('rooms test:', rooms)
+
+    return <div>
+      <CreateRoom />
+      <div>
+        {rooms}
+      </div>
+    </div>
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    rooms: state.rooms,
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(DisplayRooms)
